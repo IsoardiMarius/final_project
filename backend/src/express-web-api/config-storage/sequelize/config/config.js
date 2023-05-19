57f0bb557cb7ad48d884fe5d459a7f6e9f94deb9
mariusisoardi <misoardi13100@outlookf.fr$$$>
2023-05-19 16:40:13 +0200

    fix error sequelize db:migrate, db:create

--- backend/src/express-web-api/config-storage/sequelize/config/config.js
+++ backend/src/express-web-api/config-storage/sequelize/config/config.js
@@ -1,26 +1,29 @@
+
 const env = process.env.NODE_ENV;
 
 switch (env) {
     case 'development':
-        require('dotenv').config({ path: process.cwd() + '/.env' });
-        module.exports = {
-            username: process.env.DB_USER,
-            password: null,
-            database: process.env.DB_DATABASE,
-            host: process.env.DB_HOST,
-            dialect: process.env.DB_DIALECT // ajoutez une valeur par défaut pour le dialecte
-        };
+        require('dotenv').config({path:__dirname+'/../../../../../.env'});
         break;
     case 'test':
-        require('dotenv').config({ path: process.cwd() + '/.env.test' });
-        module.exports = {
-            username: process.env.DB_USER,
-            password: null,
-            database: process.env.DB_DATABASE,
-            host: process.env.DB_HOST,
-            dialect: process.env.DB_DIALECT
-        };
+        require('dotenv').config({path:__dirname+'/../../../../../.env.test'});
+        break;
+    case 'production':
+        require('dotenv').config({path:__dirname+'/../../../../../.env.production'});
         break;
     default:
-        throw new Error(`Unknown environment: ${env}`);
+        throw new Error('NODE_ENV environment variable must be set to "development", "test" or "production"');
+
 }
+
+module.exports = {
+            "username": process.env.DB_USER,
+            "password": null,
+            "database": process.env.DB_DATABASE,
+            "host": process.env.DB_HOST,
+            "dialect": process.env.DB_DIALECT
+}
+
+
+
+
